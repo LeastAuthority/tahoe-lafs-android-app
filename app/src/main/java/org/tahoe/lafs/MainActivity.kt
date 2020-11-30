515597a66ca2a874c6a2b24148247c07dc008620
droidizer <sowmyasguru@gmail.com>
2020-11-30 13:46:18 +0100

    Added QR code scanner initialization logic.

--- app/src/main/java/org/tahoe/lafs/MainActivity.kt
+++ app/src/main/java/org/tahoe/lafs/MainActivity.kt
@@ -1,11 +1,19 @@
 package org.tahoe.lafs
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        scan_code_button.setOnClickListener {
+            startActivity(Intent(this, ScanCodeActivity::class.java))
+        }
     }
 }
