ead968800c7d5e2ede7e3ba2fbeba566a999581f
droidizer <sowmyasguru@gmail.com>
2021-02-08 18:52:30 +0100

    Refactored wanted code.

--- app/src/main/java/org/tahoe/lafs/utils/Constants.kt
+++ app/src/main/java/org/tahoe/lafs/utils/Constants.kt
@@ -13,16 +13,12 @@ object Constants {
     const val RO_DIR_VALUE = "URI:LIT:"
     const val ONE_SECOND: Long = 1000
     const val ONE_MINUTE: Long = ONE_SECOND * 60
-    const val TWO_MINUTES: Long = ONE_MINUTE * 2
     const val ONE_HOUR: Long = ONE_MINUTE * 60
     const val ONE_DAY: Long = ONE_HOUR * 24
-    const val TWO_DAYS: Long = ONE_DAY * 2
-    const val ONE_WEEK: Long = ONE_DAY * 7
-    const val TAHOE_LAFS_FOLDER = "/tahoe-lafs/"
+    const val TAHOE_LAFS_FOLDER = "/Tahoe-LAFS/"
 }
 
 object SharedPreferenceKeys {
-    const val IS_DATA_PRESENT = "IS_DATA_PRESENT"
     const val SCANNER_URL = "SCANNER_URL"
     const val GRID_DATA = "GRID_DATA"
     const val GRID_SYNC_TIMESTAMP = "GRID_SYNC_TIMESTAMP"
